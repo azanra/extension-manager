8712f9216957a02fa8e6b442514ec0bd40dea16b
azan-personal <112457939+azanra@users.noreply.github.com>
2025-03-31 09:52:44 +0700

    Store the extension in extensionList array

---
+++ script/model/listExtension.js
@@ -0,0 +1,8 @@
+export class ListExtension {
+  constructor() {
+    this.extensionList = [];
+  }
+  addToTheList(extensionItem) {
+    this.extensionList.push(extensionItem);
+  }
+}
--- script/script.js
+++ script/script.js
@@ -1,4 +1,5 @@
 import { Extension } from "./model/extension.js";
+import { ListExtension } from "./model/listExtension.js";
 
 const firstExtension = new Extension(1, "src", "name", "desc", true);
 
@@ -6,3 +7,10 @@ console.log(firstExtension);
 firstExtension.setActive();
 console.log(firstExtension);
 console.log(firstExtension.getActive());
+
+const listExtension = new ListExtension();
+console.log(listExtension);
+
+listExtension.addToTheList(firstExtension);
+
+console.log(listExtension);
