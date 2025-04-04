15ce8a843f60655187435de6ab43106fba7ed511
azan-personal <112457939+azanra@users.noreply.github.com>
2025-04-04 09:10:08 +0700

    Add method to add multiple attribute to element

---
+++ script/view/view.js
@@ -0,0 +1,17 @@
+class View {
+  constructor() {
+    this.extensionListContainer = document.querySelector(".mainContent");
+  }
+  static setMultipleAttr(attributeObject, element, customAttribute) {
+    for (let attribute in attributeObject) {
+      if (attribute === id && customAttribute !== undefined) {
+        element.setAttribute(
+          attribute,
+          `${attributeObject[attribute]}-${customAttribute}`
+        );
+      } else {
+        element.setAttribute(attribute, attributeObject[attribute]);
+      }
+    }
+  }
+}
